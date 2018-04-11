ad9337087240fb06da8cea4c2ec95819ab8d78b9
joe lyons <jlyons10@binghamton.edu>
2018-04-11 20:09:59 +0000

    Thru part 2

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -19,12 +19,19 @@ class OracleOfBacon
   validates_presence_of :api_key
   validate :from_does_not_equal_to
 
+  KEY = '38b99ce9ec87'
+
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    if @from == @to
+      self.errors.add(:from, 'cannot be the same as To')
+    end
   end
 
   def initialize(api_key='')
-    # your code here
+    @api_key = api_key
+    @from = 'Kevin Bacon'
+    @to = 'Kevin Bacon'
+    @uri = nil
   end
 
   def find_connections
@@ -44,6 +51,7 @@ class OracleOfBacon
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+    
   end
       
   class Response
@@ -61,12 +69,32 @@ class OracleOfBacon
         parse_error_response
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      # object should have type 'unknown' and data 'unknown response' 
+      elsif ! @doc.xpath('/link').empty?            #normal graph
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?      #multiple responses / spellcheck
+        parse_spellcheck_response
+      else
+        parse_unknown_response
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+    def parse_graph_response
+      @type = :graph
+      actors = @doc.xpath('//actor')
+      movies = @doc.xpath('//movie')
+      @data = actors.zip(movies).flatten.compact.map(&:text)
+    end
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath('//match').map(&:text)
+    end
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'Unknown response type'   #might be lowercase u
     end
   end
 end
